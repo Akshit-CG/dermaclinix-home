40ad0411902211f8903c84b0e23d73692c6f4ff4
Akshit <91170781+HYakshit@users.noreply.github.com>
2026-07-10 18:05:05 +0530

    feat:hero carousal complete

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
+// hero swiper
 const heroSwiper = new Swiper(".heroSwiper", {
     loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+    },
+});
+// news icons swiper
+new Swiper(".logoSwiper", {
+    loop: true,
+    speed: 7000,
+    allowTouchMove: false,
+    slidesPerView: "auto",
+    spaceBetween: 0,
+    freeMode: {
+        enabled: true,
+        momentum: false,
+    },
+    loopAdditionalSlides: 8,
+
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
     },
 });
